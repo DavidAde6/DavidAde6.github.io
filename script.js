55c4b21ac29e1ca56b1e032aafff4c6513389951
David Adeniyi <dadeniyi54@gmail.com>
2023-07-24 15:00:12 -0500

    finalization

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElementsL.forEach((el) => observer.observe(el));
 hiddenElementsR.forEach((el) => observer.observe(el));
 hiddenElements.forEach((el) => observer.observe(el));
+
+const searchInput = document.querySelector("#searchInput")
+const cards = document.getElementsByClassName('ror')
+
+searchInput.addEventListener('keyup', function() {
+const searchValue = searchInput.value.toLowerCase();
+
+for (let i = 0; i < cards.length; i++) {
+const cardText = cards[i].textContent.toLowerCase();
+if (cardText.includes(searchValue)) {
+cards[i].style.display = 'block';
+} else {
+cards[i].style.display = 'none';
+}
+}
+});
