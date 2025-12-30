1f4d50433d70ce6578d6287ac5db40153634cf7a
David Adeniyi <dta123adeniyi@gmail.com>
2025-12-30 10:01:41 -0500

    Minor bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: mode === "production" ? "/DavidAde6.github.io/" : "/",
+}));
